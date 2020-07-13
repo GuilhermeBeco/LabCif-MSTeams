c3735b214253a0362f56b24b58d599f9d9919730
Guilherme Beco <2171293@my.ipleiria.pt>
2020-07-13 15:42:49 +0100

    license

--- EI.py
+++ EI.py
@@ -1,6 +1,7 @@
 #     LabCif-MSTeams: This project extracts MSTeams Artifacts
-#     Copyright (C) <year>  <name of author>
-#
+#     Copyright (C) 2020
+#     Authors: Guilherme Beco and Tiago Garcia
+#     Mentors: Miguel Monteiro de Sousa Frade and Patrício Rodrigues Domingues
 #     This program is free software: you can redistribute it and/or modify
 #     it under the terms of the GNU General Public License as published by
 #     the Free Software Foundation, either version 3 of the License, or
